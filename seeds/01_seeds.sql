403c7799dc4693a5928bd61cae66e18c602e1d6f
johnarvi <arvind.john@mail.utoronto.ca>
2019-11-13 15:30:22 +0000

    fixed the insert statements

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -9,19 +9,19 @@ VALUES (1 , 'Speed lamp' , 'description' , 'https://images.pexels.com/photos/208
 (2 , 'Habit mix' , 'description' , 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg?auto=compress&cs=tinysrgb&h=350' , 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg' , 460.58 , 0 , 5 , 6 , 'Canada' , '1650 Hejto Center' , 'Genwezuj' , 'Newfoundland And Labrador' , '44583' , true);
 
 INSERT INTO reservations (start_date, end_date, property_id, guest_id)
-VALUES('2018-09-11', '2018-09-11', 313, 399),
-('2019-01-04', '2019-02-01', 515, 628),
-('2021-10-01', '2021-10-14', 217, 93);
+VALUES('2018-09-11', '2018-09-11', 2, 3),
+('2019-01-04', '2019-02-01', 1, 2),
+('2021-10-01', '2021-10-14', 3, 1);
 
 INSERT INTO property_reviews (guest_id, property_id, reservation_id, rating, message)
-VALUES(2, 5, 10, 3, 'message'),
-(1, 4, 1, 4, 'message'),
-(8, 1, 2, 4, 'message');
+VALUES(2, 2, 1, 3, 'message'),
+(1, 2, 1, 4, 'message'),
+(2, 1, 2, 4, 'message');
 
-INSERT INTO guest_reviews (guest_id, owner_id, property_id, reservation_id, rating, message)
-VALUES(1, 2, 1, 1, 3, 'message'),
-(2, 1, 1, 2, 4, 'message'),
-(1, 2, 1, 3, 5, 'message');
+INSERT INTO guest_reviews (guest_id, owner_id, reservation_id, rating, message)
+VALUES(1, 2, 1, 3, 'message'),
+(2, 1, 2, 4, 'message'),
+(1, 2, 3, 5, 'message');
 
 INSERT INTO rates (start_date, end_date, cost_per_night, property_id)
 VALUES('018-09-11', '2018-09-11', 930.61, 1),
