c2767a805b90ada1acaa5336f586db7bf7d536da
johnarvi <arvind.john@mail.utoronto.ca>
2019-11-14 19:33:05 +0000

    implemented the getAllProperties function using queries and sql

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -93,7 +93,6 @@ const getAllReservations = function(guest_id, limit = 10) {
   LIMIT 10;
   `, [guest_id])
     .then(res => {
-      console.log(res.rows);
       return (res.rows.length > 0) ? res.rows : null;
     });
 };
@@ -108,11 +107,49 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  return pool.query(`
-  SELECT * 
+  const queryParams = [];
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
   FROM properties
-  LIMIT $1
-  `, [limit])
+  JOIN property_reviews ON properties.id = property_id 
+  WHERE true`;
+
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += ` AND city LIKE $${queryParams.length} `;
+  }
+
+  if (options.owner_id) {
+    queryParams.push(`${options.owner_id}`);
+    queryString += ` AND owner_id = $${queryParams.length} `;
+  }
+
+  if (options.minimum_price_per_night) {
+    queryParams.push(`${options.minimum_price_per_night * 100}`);
+    queryString += ` AND cost_per_night >= $${queryParams.length} `;
+  }
+
+  if (options.maximum_price_per_night) {
+    queryParams.push(`${options.maximum_price_per_night * 100}`);
+    queryString += ` AND cost_per_night <= $${queryParams.length} `;
+  }
+
+  if (options.minimum_rating) {
+    queryParams.push(`${options.minimum_rating}`);
+    queryString += ` AND rating >= $${queryParams.length} `;
+  }
+
+  queryParams.push(limit);
+  queryString += `
+  GROUP BY properties.id
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+
+  //console.log('------------------------youuuuuu');
+  //console.log(queryString, queryParams);
+  
+  return pool.query(queryString, queryParams)
     .then(res => res.rows);
 };
 
