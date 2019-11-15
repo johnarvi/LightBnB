6f95173cd4028dd42c41bc94e41f4918c4816b36
johnarvi <arvind.john@mail.utoronto.ca>
2019-11-15 00:21:57 +0000

    fixed issues with the with returning more than 10 results

--- LightBnB_WebApp/server/apiRoutes.js
+++ LightBnB_WebApp/server/apiRoutes.js
@@ -1,7 +1,7 @@
 module.exports = function(router, database) {
 
   router.get('/properties', (req, res) => {
-    database.getAllProperties(req.query, 10)
+    database.getAllProperties(req.query)
       .then(properties => res.send({properties}))
       .catch(e => {
         console.error(e);
